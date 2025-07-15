a7600006e01a84855d3681e7ade1786c33b891ea
MohamedMohamady125 <mohamadmohamady125@gmail.com>
2025-07-15 06:58:48 +0300

    Add debug logging and fallback values for database config

--- app/config.py
+++ app/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DB_HOST = os.getenv("DB_HOST")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = os.getenv("DB_HOST", "mysql.railway.internal")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "omqxUvCPxFkGeCYjMYzfylckhYzcFwWV")
+    DB_NAME = os.getenv("DB_NAME", "railway")
     JWT_SECRET = os.getenv("JWT_SECRET")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 
